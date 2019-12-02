18f53e32543f8de03eb3286dad3f85e6a6dbee1f
bryce.haire <bryce.haire@pointsbet.com>
2019-12-02 08:48:11 -0700

    Day 1 + Day 2

---
+++ Day1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputs = File
+                                    .ReadAllLines("C:\\Users\\bryce.haire\\AoC\\Day1\\input.txt")
+                                    .Select(s => int.Parse(s))
+                                    .ToList();           
+            List<int> answer1 = new List<int>();
+            List<int> answer2 = new List<int>();
+            foreach(var input in inputs)
+            {
+                var modulo = input % 3;
+                var calculate = ((input - modulo) / 3) -2;
+                answer1.Add(calculate);
+            }
+
+            Console.WriteLine($"Part 1 Answer: {answer1.Sum()}");
+
+            foreach(var input in inputs)
+            {
+                var modulo = input % 3;
+                var calculate = ((input - modulo) / 3) -2;
+                answer2.Add(calculate);
+                while(calculate > 3)
+                {
+                    modulo = calculate % 3;
+                    calculate = ((calculate - modulo) / 3) -2;
+                    if(calculate > 0)
+                    {
+                        answer2.Add(calculate);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Part 2 Answer: {answer2.Sum()}");
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> originals = File
+                                    .ReadAllText("C:\\Users\\bryce.haire\\AoC\\Day2\\input.txt")
+                                    .Split(',')
+                                    .Select(s => int.Parse(s))
+                                    .ToList();  
+            List<int> inputs = originals.ToList();
+
+            for(int index = 0; index < inputs.Count; index = index + 4)
+            {
+                if(index == 0)
+                {
+                    inputs[1] = 12;
+                    inputs[2] = 2;
+                }
+                var opCode = inputs[index];
+                if(opCode == 99)
+                {
+                    break;
+                }
+                var index1 = inputs[index + 1];
+                var index2 = inputs[index + 2];
+                var index3 = inputs[index + 3];
+
+                var value1 = inputs[index1];
+                var value2 = inputs[index2];
+                var output = opCode == 1 ? value1 + value2 : value1 * value2;
+                inputs[index3] = output;
+            }
+            Console.WriteLine($"Part 1 Answer: {inputs[0]}");
+
+            for(int noun = 0; noun < 100; noun++)
+            {
+                for(int verb = 0; verb < 100; verb++)
+                {
+                    inputs = originals.ToList();
+                    for(int index = 0; index < originals.Count; index = index + 4)
+                    {
+                        if(index == 0)
+                        {
+                            inputs[1] = noun;
+                            inputs[2] = verb;
+                        }
+                        var opCode = inputs[index];
+                        if(opCode == 99)
+                        {
+                            if(inputs[0] == 19690720)
+                            {
+                                Console.WriteLine($"Part 2 Answer: {100 * noun + verb}");
+                            }
+
+                            break;
+                        }
+                        var index1 = inputs[index + 1];
+                        var index2 = inputs[index + 2];
+                        var index3 = inputs[index + 3];
+
+                        var value1 = inputs[index1];
+                        var value2 = inputs[index2];
+                        var output = opCode == 1 ? value1 + value2 : value1 * value2;
+                        inputs[index3] = output;
+                    }
+                }
+            }
+        }
+    }
+}
